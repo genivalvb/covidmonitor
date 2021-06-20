ae5e94fa48bbc912b3dd95b57410f07009d65cee
Genival Silva <genivalvb@yahoo.com.br>
2021-06-20 20:36:36 -0300

    CovidMonitor Project

---
+++ src/main/java/com/cpujob/covidmonitor/CovidmonitorApplication.java
@@ -0,0 +1,13 @@
+package com.cpujob.covidmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidmonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidmonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cpujob/covidmonitor/CovidmonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpujob.covidmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidmonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
